119016167a153683d164ab587b00917a67110224
JCPrime01 <vanzokiller@gmail.com>
2026-06-29 16:57:05 -0300

    chore: shareCode 60MDU733SGM -> 72AAJ7SMKDC (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/dgukyqey?shareCode=60MDU733SGM&afp=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/dgukyqey?shareCode=72AAJ7SMKDC&afp=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
