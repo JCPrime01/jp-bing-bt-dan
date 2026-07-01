05a636043d1cbb69b90a57838dcaae244dba5889
JCPrime01 <vanzokiller@gmail.com>
2026-07-01 10:09:34 -0300

    chore: shareCode EX1EVNK21MR -> KMSBCN2AB24 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=EX1EVNK21MR'
+    const shareCode = '&shareCode=KMSBCN2AB24'
     
     const afp = '&afp=bingo'
 
