d2961755b6eed4e2f3d70ee7be67b80c4787c58a
JCPrime01 <vanzokiller@gmail.com>
2026-07-10 09:24:03 -0300

    chore: shareCode UH9RCMPC38T -> L3NIDD479W5 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=UH9RCMPC38T'
+    const shareCode = '&shareCode=L3NIDD479W5'
     
     const afp = '&afp=bingo'
 
