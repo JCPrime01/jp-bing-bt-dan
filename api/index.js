05deaadd28605e8e57b97c32d08d2dbcccda541c
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 12:03:00 -0300

    chore: shareCode L3NIDD479W5 -> NCMAB46OLHG (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=L3NIDD479W5'
+    const shareCode = '&shareCode=NCMAB46OLHG'
     
     const afp = '&afp=bingo'
 
