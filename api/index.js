e1b56396570e0e78b4e583b8d968a7be308c75a3
JCPrime01 <vanzokiller@gmail.com>
2026-07-29 10:50:02 -0300

    chore: shareCode 020OTRQGSZL -> R8FQNWTREUH (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=020OTRQGSZL'
+    const shareCode = '&shareCode=R8FQNWTREUH'
     
     const afp = '&afp=bingo'
 
