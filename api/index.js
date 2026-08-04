4a5bc9eabacd92047fc8988c31cf6c2bfcf73df5
JCPrime01 <vanzokiller@gmail.com>
2026-08-04 11:16:31 -0300

    chore: shareCode M54BT78BN0T -> 8COYFUE7DUZ (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=M54BT78BN0T'
+    const shareCode = '&shareCode=8COYFUE7DUZ'
     
     const afp = '&afp=bingo'
 
