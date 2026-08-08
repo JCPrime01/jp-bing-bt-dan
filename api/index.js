49afa9335e23e1c61ab6f47a9bb664e6b3d8aa8f
JCPrime01 <vanzokiller@gmail.com>
2026-08-08 13:02:23 -0300

    chore: shareCode FTU97CLE7FJ -> ZHWTVZQZ40R (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=FTU97CLE7FJ'
+    const shareCode = '&shareCode=ZHWTVZQZ40R'
     
     const afp = '&afp=bingo'
 
